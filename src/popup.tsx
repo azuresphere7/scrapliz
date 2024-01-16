26a707b69aa91c5c9d215da04d3c74f721397a43
azuresphere7 <dustin.jason.lee@gmail.com>
2024-01-16 12:01:27 +0100

    changed file formatting

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,55 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { loggiz } from './utils'
-import './scss/body.scss'
+import React, { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { loggiz } from "./utils";
+import "./scss/body.scss";
 
 const Popup = () => {
-			const [count, setCount] = useState(0)
-   const [currentURL, setCurrentURL] = useState<string>()
+    const [count, setCount] = useState(0);
+    const [currentURL, setCurrentURL] = useState<string>();
 
-   useEffect(() => {
-      chrome.action.setBadgeText({ text: count.toString() })
-   }, [count])
+    useEffect(() => {
+        chrome.action.setBadgeText({ text: count.toString() });
+    }, [count]);
 
-   useEffect(() => {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-         setCurrentURL(tabs[0].url)
-      })
-   }, [])
+    useEffect(() => {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            setCurrentURL(tabs[0].url);
+        });
+    }, []);
 
-   const changeBackground = () => {
-      loggiz()
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-         const tab = tabs[0]
-         if (tab.id) {
-            chrome.tabs.sendMessage(tab.id, { color: '#555555' }, (msg) => {
-               console.log('result message:', msg)
-            })
-         }
-      })
-   }
+    const changeBackground = () => {
+        loggiz();
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            const tab = tabs[0];
+            if (tab.id) {
+                chrome.tabs.sendMessage(tab.id, { color: "#555555" }, (msg) => {
+                    console.log("result message:", msg);
+                });
+            }
+        });
+    };
 
-   return (
-						<>
-         <ul style={{ minWidth: '700px' }}>
-            <li>Current URL: {currentURL}</li>
-            <li>Current Time: {new Date().toLocaleTimeString()}</li>
-         </ul>
-         <button
-            onClick={() => setCount(count + 1)}
-            style={{ marginRight: '5px' }}
-         >
-            count up
-         </button>
-         <button onClick={changeBackground}>change background</button>
-      </>
-   )
-}
+    return (
+        <>
+            <ul style={{ minWidth: "700px" }}>
+                <li>Current URL: {currentURL}</li>
+                <li>Current Time: {new Date().toLocaleTimeString()}</li>
+            </ul>
+            <button
+                onClick={() => setCount(count + 1)}
+                style={{ marginRight: "5px" }}
+            >
+                count up
+            </button>
+            <button onClick={changeBackground}>change background</button>
+        </>
+    );
+};
 
-const root = createRoot(document.getElementById('root')!)
+const root = createRoot(document.getElementById("root")!);
 
 root.render(
-   <React.StrictMode>
-      <Popup />
-   </React.StrictMode>
-)
+    <React.StrictMode>
+        <Popup />
+    </React.StrictMode>
+);
